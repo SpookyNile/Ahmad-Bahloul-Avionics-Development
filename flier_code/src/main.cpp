a9925f4bf8c3f1b5ce2e5139022563524e103c28
Youssef Bahloul <ybahloul77@gmail.com>
2025-06-06 17:55:36 -0400

    Youssef branch

---
+++ flier_code/src/main.cpp
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#include <LoRa.h>
+#include <SPI.h>
+
+#include <mpu.h>
+#include <gps.h>
+#include <baro.h>
+
+int counter = 0;
+
+void setup()
+{
+  Serial.begin(115200); //Serial monitor
+  Serial.println("LoRa Sender");
+
+  if (!LoRa.begin(915E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
+  // setupMPU();
+  // setupGPS();
+  // setupBarometer();
+
+  // delay(100);
+  
+}
+
+void loop()
+{
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+
+  // delay(5000);
+  // loopMPU();
+  // loopGPS();
+  // loopBarometer();
+  // delay(500);
+}
+
